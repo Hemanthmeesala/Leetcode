2c26101232f94bb57060920ca664519c92dc4368
Hemanthmeesala <135572539+Hemanthmeesala@users.noreply.github.com>
2024-06-13 11:48:38 +0530

    Time: 2 ms (69.59%), Space: 11.9 MB (48.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-
-    bool binSearch(vector<vector<int>>& matrix,int target,int row,int col){
-        int s=0;
-        int e=col-1;
-
-        int mid=s+(e-s)/2;
-
-        while(s<=e){
-            if(matrix[row][mid]==target){
-                return 1;
-            }else if(matrix[row][mid]>target){
-                e=mid-1;
-            }else{
-                s=mid+1;
-            }
-            mid=s+(e-s)/2;
-        }
-        return 0;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size();
-        int col = matrix[0].size();
+        if(matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
 
-        for(int i=0;i<row-1;i++){
-            if(matrix[i][0]<=target && matrix[i+1][0]>target){
-                return binSearch(matrix,target,i,col);
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int i = 0;  
+        int j = cols - 1;  
+        while(i < rows && j >= 0) {
+            if(matrix[i][j] == target) {
+                return true;  // Target found
+            } else if(matrix[i][j] > target) {
+                j--;  
+            } else {
+                i++; 
             }
         }
 
-        if(matrix[row-1][0]<=target && matrix[row-1][col-1]>=target){
-            return binSearch(matrix,target,row-1,col);
-        }
-        return 0;
-
+        return false;  
     }
 };
